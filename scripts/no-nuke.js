0a1a604a2132284ea791597e98a843111e6f301a
Jiamu Sun <barroit@linux.com>
2025-08-17 14:03:00 +0900

    package: prevent react router nuking build dir
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ scripts/no-nuke.js
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+import { execSync } from 'node:child_process'
+
+function prebuild()
+{
+	execSync('./scripts/init-shared.sh', { stdio: 'inherit' })
+}
+
+export default function no_nuke()
+{
+	return {
+		name:    'No Fucking Nuke',
+		version: '0.0.39',
+
+		buildStart: prebuild,
+	}
+}
--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,14 @@ import devtoolsJson from 'vite-plugin-devtools-json'
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 
+import no_nuke from './scripts/no-nuke.js'
+
 export default defineConfig({
 	plugins: [
 		devtoolsJson(),
 		reactRouter(),
 		tailwindcss(),
+		no_nuke(),
 	],
 	publicDir: 'assets',
 	build: {
